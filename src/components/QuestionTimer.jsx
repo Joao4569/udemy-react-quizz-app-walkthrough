71685cd5923024484d1c6a19ce99f271f8a67f56
João Rodrigues <johnnyrod4569@gmail.com>
2024-03-18 17:37:37 +0100

    learn: Work with Effect Dependencies and useCallBack hook.

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import { useEffect } from "react";
-
+import { useState, useEffect } from "react";
 export default function QuestionTimer({ timeout, onTimeout }) {
 
 	const [remainingTime, setRemainingTime] = useState(timeout);
 
 	useEffect(() => {
+		console.log('SETTING TIMEOUT');
 		setTimeout(onTimeout, timeout);
 	}, [onTimeout, timeout]);
 
 	useEffect(() => {
+		console.log('SETTING INTERVAL');
 		setInterval(() =>{
 			setRemainingTime(prevRemainingTime => prevRemainingTime - 100)
 		}, 100);
