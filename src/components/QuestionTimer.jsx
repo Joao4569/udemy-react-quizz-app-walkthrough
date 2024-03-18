3594e29b0e7889c02ecd3ed2f1a75f31e32aefb3
João Rodrigues <johnnyrod4569@gmail.com>
2024-03-18 17:58:59 +0100

    chore: Add comments for referall in future

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,25 +1,28 @@
 import { useState, useEffect } from "react";
 export default function QuestionTimer({ timeout, onTimeout }) {
 
+	// handle the state of the remaining time
 	const [remainingTime, setRemainingTime] = useState(timeout);
 
+	// handle the timet and the timeout
 	useEffect(() => {
 		console.log('SETTING TIMEOUT');
 		 const timer = setTimeout(onTimeout, timeout);
 
-		 // Cleanup function
+		 // timer cleanup function
 		return () => {
 			clearTimeout(timer);
 		};
 	}, [onTimeout, timeout]);
 
+	// handle the interval
 	useEffect(() => {
 		console.log('SETTING INTERVAL');
 		const interval = setInterval(() =>{
 			setRemainingTime(prevRemainingTime => prevRemainingTime - 100)
 		}, 100);
 
-		// Cleanup function
+		// interval cleanup function
 		return () => {
 			clearInterval(interval);
 		};
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,20 +5,28 @@ import QuestionTimer from "./QuestionTimer.jsx";
 import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
+
+  // manage the user's answers
   const [userAnswers, setUserAnswers] = useState([]);
 
+  // get the index of the current question
   const activeQuestionIndex = userAnswers.length;
 
+  // check if the quiz is complete
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
+  // handle the user's answer selection
   const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }, []);
 
+  // handle the user's answer skip
   const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
+
+  // render the quiz complete message
   if (quizIsComplete) {
     return (
       <div id="summary">
@@ -31,18 +39,23 @@ export default function Quiz() {
     );
   }
 
+  // shuffle the answers for the current question [correct answer is always the first answer in the array, in the questions.js file]
 	const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
 	shuffledAnswers.sort(() => Math.random() - 0.5);
 
+  // render the quiz component with the current question
   return (
     <div id="quiz">
       <div id="question">
+
         <QuestionTimer
           key={activeQuestionIndex} // When the key changes then the component is unmounted and remounted.
           timeout ={10000}
           onTimeout={handleSkipAnswer}
         />
+
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
@@ -52,6 +65,7 @@ export default function Quiz() {
             </li>
           ))}
         </ul>
+
       </div>
     </div>
   );
